72db9c1e93fd495a586605e3455ba9cdaceaf4e4
Prateek Ranjan <prateekranjan14059827@gmail.com>
2020-07-06 13:31:11 +0530

    The Food Runner App

---
+++ app/src/main/java/com/prateek/foodrunner/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.prateek.foodrunner
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler().postDelayed({
+            val intent= Intent(this,LoginActivity::class.java)
+            startActivity(intent)},2000)
+    }
+}
